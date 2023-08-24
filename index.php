e92e5698c2239822c08fef11223a54b0dc241e6f
ValeriaLeonH <valerialeon837@gmail.com>
2023-08-24 08:22:57 -0400

    feat: guardar

--- index.php
+++ index.php
@@ -2,23 +2,19 @@
   include("database/db.php"); //linkeo de basededatos
 ?>
 
-
-
 <?php
   include("includes/header.php"); //linkeo de header 
 ?>
 
-  <main class="container p-4">
-    <div class="row">
-      <div class="col-md-4">
-        <!-- MESSAGES -->
-        
-        <?php if(isset($_SESSION['message']))
-        ?>
-        <div class="alert alert-<?= $_SESSION ['message_type']?> alert-dismissible fade show" role="alert"></div>
-      </div>
+<main class="container p-4">
+  <div class="row">
+    <div class="col-md-4">
+      <!-- MESSAGES -->
+      <?php if(isset($_SESSION['message']))?>
+      <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert"></div>
     </div>
-  </main>
+  </div>
+</main>
 
 <?php
   include("includes/footer.php");
